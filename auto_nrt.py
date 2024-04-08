d01a63fb4485b8ed004c47ee53109cceeca194fc
Leonardo Lopes <91706429+LeonardoLopes16@users.noreply.github.com>
2024-04-08 11:53:29 -0300

    APRIMORANDO A INSERÇÃO DE ALUNOS NA NRT

--- auto_nrt.py
+++ auto_nrt.py
@@ -1,68 +1,93 @@
 import pyautogui
 
+import time
+from datetime import datetime
+
 qnt = int(input("Digite a quantidade de alunos que vão ser cadastrados: "))
 cont = 0
-nrt = (input('Digite a NRT: '))
+# nrt = (input('Digite a NRT: '))
 
 
 
-pyautogui.click(839, 19,duration=0.5)
-for i in range(11):
-    pyautogui.keyDown('tab')
-    i+1
+# # SACI
+# pyautogui.click(517,26, duration=0.5)
+# pyautogui.hotkey('ctrl', 'home')
+        
+#     # SISHAB --> NRT --> ENIVIADAS
+# pyautogui.moveTo(195,508,duration=0.3)
+# pyautogui.moveTo(252,502,duration=0.3)
+# pyautogui.click(309,510,duration=0.3)
 
-pyautogui.write(nrt)
-
-# # Página excel
-# pyautogui.click(660, 20,duration=0.5)
+# time.sleep(2)
 
 
-# pyautogui.click(406, 412,duration=0.5)
-# pyautogui.hotkey('ctrl', 'c')
+# for i in range(11):
+#     pyautogui.keyDown('tab')
+#     i+1
+# pyautogui.write(nrt)
+# pyautogui.keyDown('tab')
+# pyautogui.write('2024')
+# pyautogui.keyDown('enter')
 
-# # Página Anac
-# pyautogui.click(839, 19,duration=0.5)
+# time.sleep(1.5)
+# for _ in range(25):
+#     pyautogui.press('tab')
+# pyautogui.hotkey('enter')
 
-# pyautogui.click(436, 461,duration=0.7)
+# Página excel
+pyautogui.click(278, 20,duration=0.5)
+pyautogui.hotkey('ctrl', 'home')
 
-# pyautogui.click(478, 309,duration=0.5)
 
-# pyautogui.click(542, 335,duration=0.5)
-# pyautogui.hotkey('ctrl', 'v')
+pyautogui.click(406, 412,duration=0.5)
+pyautogui.hotkey('ctrl', 'c')
 
-# pyautogui.click(498, 396,duration=0.2)
+# Página Anac
+pyautogui.click(517,26)
 
-# pyautogui.click(634, 540,duration=0.5)
+pyautogui.click(436, 461,duration=0.7)
 
-# pyautogui.click(660, 20,)
+pyautogui.click(478, 309)
 
-# qnt = qnt-1
+pyautogui.click(542, 335)
+pyautogui.hotkey('ctrl', 'v')
 
-# while cont < qnt:
-#     pyautogui.keyDown('down')
-#     pyautogui.hotkey('ctrl', 'c')
+pyautogui.click(498, 396,duration=0.2)
 
-#     pyautogui.click(839, 19,duration=0.5)
+pyautogui.click(634, 540)
 
-#     pyautogui.click(436, 461,duration=0.7)
+pyautogui.click(278, 20)
 
-#     pyautogui.click(478, 309,duration=0.5)
+qnt = qnt-1
 
-#     pyautogui.click(542, 335,duration=0.5)
-#     pyautogui.hotkey('ctrl', 'v')
+while cont < qnt:
+    pyautogui.keyDown('down')
+    pyautogui.hotkey('ctrl', 'c')
 
-#     pyautogui.click(498, 396,duration=0.2)
+    pyautogui.click(517, 19,duration=0.2)
 
-#     pyautogui.click(634, 540,duration=0.5)
+    pyautogui.click(436, 461,duration=0.3)
 
-#     pyautogui.click(660, 20,)
+    pyautogui.click(478, 309,duration=0.2)
+
+    pyautogui.click(542, 335)
+    pyautogui.hotkey('ctrl', 'v')
+
+    pyautogui.click(498, 396,duration=0.1)
+    
+
+    pyautogui.click(634, 540,duration=0.1)
+    pyautogui.click(634, 540)
+    
+
+    pyautogui.click(278, 20,duration=0.4)
 
     
-#     cont = cont + 1
-#     if qnt == cont:
-#         break
+    cont = cont + 1
+    if qnt == cont:
+        break
     
-# pyautogui.alert('Alunos cadastrados com ')
+pyautogui.alert('Alunos cadastrados com sucesso!')
 
 
 
