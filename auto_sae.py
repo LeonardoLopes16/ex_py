177af14362e1c4b74db3850fc277bc5e68a481d4
Leonardo Lopes <91706429+LeonardoLopes16@users.noreply.github.com>
2024-04-12 13:59:55 -0300

    desenvolvendo lógica da nova automação

---
+++ auto_sae.py
@@ -0,0 +1,11 @@
+# SISHAB --> SAE --> ENIVIADAS
+# INCLUIR
+# ANO NRT // NÚMERO NRT // NOME DO ALUNO(scroll)
+# LOCALIZAÇÃO
+# TIPO --> DATA DO EXAME --> PAÍS
+# ENDEREÇO + f'{HH/MM AS HH/MM}
+# ESTADO 
+# CIDADE (write sao p + press enter)
+# button escolher examinador --> (x,y)windown in upside left
+# CRTL F --> WRITE NAME XQ E --> SELECT and press enter
+# avançar
